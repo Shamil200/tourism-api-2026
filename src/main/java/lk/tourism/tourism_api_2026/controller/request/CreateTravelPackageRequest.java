7ac06dad7af5e93664e6642ac86ae9c8fbaa4e12
2000sans <nuwantha2000w@gmail.com>
2026-02-18 13:17:50 +0530

    edited 'CreateTravelPackageRequest' request class

--- src/main/java/lk/tourism/tourism_api_2026/controller/request/CreateTravelPackageRequest.java
+++ src/main/java/lk/tourism/tourism_api_2026/controller/request/CreateTravelPackageRequest.java
@@ -1,9 +1,7 @@
 package lk.tourism.tourism_api_2026.controller.request;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import tools.jackson.databind.PropertyNamingStrategies;
 import tools.jackson.databind.annotation.JsonNaming;
@@ -18,15 +16,22 @@ public class CreateTravelPackageRequest {
     private String name;
 
     @NotNull(message = "people count cannot be empty")
+    @Positive(message = "people count must be positive")
     private Integer peopleCount;
 
     @NotBlank(message = "duration cannot be empty")
     private String duration;
 
     @NotNull(message = "total price cannot be null")
+    @Positive(message = "total price must be positive")
+    @DecimalMin(value = "0.0", inclusive = false, message = "total price must be greater than 0")
+    @DecimalMax(value = "40000.0", inclusive = false, message = "total price must be less 40,000")
     private Double totalPrice;
 
     @NotNull(message = "admission percentage cannot be empty")
+    @Positive(message = "admission percentage must be positive")
+    @DecimalMin(value = "0.0", inclusive = false, message = "admission percentage must be greater than 0")
+    @DecimalMax(value = "100.0", inclusive = false, message = "admission percentage must be less than 100")
     private Float admissionPercentage;
 
     @NotEmpty(message = "visiting locations cannot be empty")
